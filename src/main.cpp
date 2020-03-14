d04bbff021483ad144cd2c9a2fee58488d585f6d
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-03-14 12:40:43 +0900

    テストの整備など

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
-int main( int argc, char const * argv[] )
-{
-  int n = argc<2 ? 100 : std::atoi(argv[1]);
-  std::cout << n << std::endl;
+#if !defined TESTBUILD
+
+#include "sum_of_digits.h"
+
+int main(int argc, char const *argv[]) {
+  int a = argc < 2 ? 1234567 : std::atoi(argv[1]);
+  int b = argc < 3 ? 10 : std::atoi(argv[2]);
+
+  std::cout << "num=" << a << "\n"
+            << "base=" << b << "\n"
+            << "sum of each digits: " << sum_of_digits(a, b) << std::endl;
 #if defined DEBUG
-  std::cout <<   "DEBUG!" << std::endl;
+  std::cout << "DEBUG!" << std::endl;
 #endif
 #if defined _DEBUG
-  std::cout <<   "_DEBUG!" << std::endl;
+  std::cout << "_DEBUG!" << std::endl;
 #endif
 #if defined NDEBUG
-  std::cout <<   "NOT DEBUG!" << std::endl;
-#endif
-#if defined TEST
-  std::cout <<   "TEST!" << std::endl;
+  std::cout << "NDEBUG!" << std::endl;
 #endif
 }
+
+#endif
---
+++ src/sum_of_digits.cpp
@@ -0,0 +1,7 @@
+#include <exception>
+#include "sum_of_digits.h"
+
+int sum_of_digits( int a, int b )
+{
+  return 1;
+}
