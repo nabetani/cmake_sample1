0ff29f27b4ba4db3ba540fa2e052ec2e2ecc6a1b
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-03-14 12:56:33 +0900

    非正常系の実装

--- src/sum_of_digits.cpp
+++ src/sum_of_digits.cpp
@@ -1,7 +1,10 @@
 #include "sum_of_digits.h"
-#include <exception>
+#include <stdexcept>
 
 int sum_of_digits(int a, int b) {
+  if (a < 0 || b < 2) {
+    throw std::runtime_error("invalid param in sum_of_digits");
+  }
   int sum = 0;
   while (a != 0) {
     sum += a % b;
