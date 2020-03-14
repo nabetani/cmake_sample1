9a9cb5125c5c215402fc846ef2e0c1edfa9d16f6
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-03-14 12:56:45 +0900

    例外処理を入れる

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <exception>
 #include <iostream>
 
 #if !defined TESTBUILD
@@ -6,21 +7,25 @@
 #include "sum_of_digits.h"
 
 int main(int argc, char const *argv[]) {
-  int a = argc < 2 ? 1234567 : std::atoi(argv[1]);
-  int b = argc < 3 ? 10 : std::atoi(argv[2]);
+  try {
+    int a = argc < 2 ? 1234567 : std::atoi(argv[1]);
+    int b = argc < 3 ? 10 : std::atoi(argv[2]);
 
-  std::cout << "num=" << a << "\n"
-            << "base=" << b << "\n"
-            << "sum of each digits: " << sum_of_digits(a, b) << std::endl;
+    std::cout << "num=" << a << "\n"
+              << "base=" << b << "\n"
+              << "sum of each digits: " << sum_of_digits(a, b) << std::endl;
 #if defined DEBUG
-  std::cout << "DEBUG!" << std::endl;
+    std::cout << "DEBUG!" << std::endl;
 #endif
 #if defined _DEBUG
-  std::cout << "_DEBUG!" << std::endl;
+    std::cout << "_DEBUG!" << std::endl;
 #endif
 #if defined NDEBUG
-  std::cout << "NDEBUG!" << std::endl;
+    std::cout << "NDEBUG!" << std::endl;
 #endif
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
 }
 
 #endif
